0b794c84e14ee26b63e4cf51ab6f2b8b9f67114f
= <sriramr083@gmail.com>
2018-06-01 21:51:31 +0530

    added routes to activate and deactivate a cook

--- backend/server/routes/cook.js
+++ backend/server/routes/cook.js
@@ -29,4 +29,58 @@ router.post('/fcmToken/:token', authenticateCook, (req,res) => {
   });
 });
 
+ router.post('/activate', authenticateCook, (req,res) => {
+   const cook = req.cook;
+   if (cook.isActive) {
+     return res.status(400).send({
+       status: '400',
+       error: 'Cook already active'
+     });
+   }
+  Cook.updateOne({
+    _id: cook._id
+  },{
+    $set: {
+      isActive: true
+    }
+  }).then((cook) => {
+    res.status(200).send({
+      status: 'OK',
+      error: null
+    });
+  }).catch((error) => {
+    res.status(400).send({
+      status: '400',
+      error: 'Unable to activate cook. Check the token.'
+    });
+  });
+ });
+
+ router.post('/deactivate', authenticateCook, (req,res) => {
+  const cook = req.cook;
+  if (!cook.isActive) {
+    return res.status(400).send({
+      status: '400',
+      error: 'Cook already deactivated'
+    });
+  }
+ Cook.updateOne({
+   _id: cook._id
+ },{
+   $set: {
+     isActive: false
+   }
+ }).then((cook) => {
+   res.status(200).send({
+     status: 'OK',
+     error: null
+   });
+ }).catch((error) => {
+   res.status(400).send({
+     status: '400',
+     error: 'Unable to deactivate cook. Check the token.'
+   });
+ });
+ });
+
 module.exports = router;
