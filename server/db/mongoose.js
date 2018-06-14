eb612ccaa602b4dc72e19bf022a1166392c373c7
= <sriramr083@gmail.com>
2018-06-14 18:19:05 +0530

    handled rejection in mongoose connect

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/FoosiahDB');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/FoosiahDB').catch(e => console.log(e));
 
 module.exports = { mongoose };
