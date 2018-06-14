53a2d070ee171a0d4cff8b19587d06aec24a6515
= <sriramr083@gmail.com>
2018-06-14 18:16:38 +0530

    added about route

--- server/server.js
+++ server/server.js
@@ -23,6 +23,10 @@ app.get('/', (req,res) => {
   res.send("Welcome to foosiah");
 });
 
+app.get('/about', (req,res) => {
+  res.send('This is the about page');
+})
+
 // routes to register cook or customer
 app.use('/register', registerRoutes);
 
